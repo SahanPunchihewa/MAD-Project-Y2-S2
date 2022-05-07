e3baf0da2c31f7ee1a32f2d4a325726d6991deaf
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-05-07 15:40:01 +0530

    Manager Login Page Intent Complete

--- app/src/main/java/com/example/mobileapplication/LoginActivity.java
+++ app/src/main/java/com/example/mobileapplication/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.mobileapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public Button managerLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        managerLogin = findViewById(R.id.Login_Manager_btn);
+        managerLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, ManagerLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
