6e6b66ffb80a8aebb6974d8479ccc9a686fdb58a
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-05-10 21:07:40 +0530

    Customer Model Class Implementation

--- app/src/main/java/com/example/mobileapplication/models/CustomerModel.java
+++ app/src/main/java/com/example/mobileapplication/models/CustomerModel.java
@@ -9,5 +9,63 @@ public class CustomerModel {
     String Password;
     String Cpassword;
 
+    public CustomerModel() {};
 
+    public CustomerModel(String id, String Name,String EmailAddress,String PhoneNumber, String Password,String Cpassword) {
+
+        this.id = id;
+        this.Name = Name;
+        this.EmailAddress = EmailAddress;
+        this.PhoneNumber = PhoneNumber;
+        this.Password = Password;
+        this.Cpassword = Cpassword;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public void setEmailAddress(String EmailAddress) {
+        this.EmailAddress = EmailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getCpassword() {
+        return Cpassword;
+    }
+
+    public void setCpassword(String Cpassword) {
+        this.Cpassword = Cpassword;
+    }
 }
